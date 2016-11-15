842f936dc68252a6ecf47ce40c8e418f9f7b583c
kevincaro <kevin_takla@hotmail.com>
2016-11-14 22:41:44 -0500

    partie JSON marche

--- map.js
+++ map.js
@@ -4,19 +4,20 @@
 	initMap(45.633331,-72.550003);
     
     	var objects = {};
-	var availableTags = [];
+	var availableTags = [];	
 
-	$.getJSON( "villes.json", function( data ) {
+	$.getJSON( "villes.json", function( data , objects ) {			
+		var o = {};		
 		$.each(data, function (key, value) {
-			objects[key]=value; //push values here
+			o[key]=value; 
 			availableTags.push(key);
 		});
-		
-		$( "#tags" ).autocomplete({
-			source: availableTags
-		});
-		console.log(ville)
+		envoieData(o);
 	});
+
+	function envoieData(o) {
+		objects = o;
+	}
     
 	// $ est un raccourci pour jQuery
 	// A l'aide de $, vous pouvez chercher n'importe quel
@@ -32,8 +33,8 @@
 	$("#btnfrancais").click(function(){
 		$('h1').html("Retrouve une ville du Quebec!");
 		$("p").html("Entrez une ville pour la retrouver sur la carte Google Maps!");
-		alert(object[$("#txtarea").val()].lon);	
-		//initMap($("#txtarea").val()),object);
+		var ville = $("#txtarea").val();
+		initMap(objects[ville].lat,objects[ville].lon)
 		// Ceci cache tous les paragraphes sur la page
 		$("p").hide();
 	});
@@ -41,28 +42,13 @@
 	$("#btnanglais").click(function(){
 		 $('h1').html("Find a city in Quebec!");
 		 $("p").html("Enter a city to find it on Google maps");
-		// Ceci cache tous les paragraphes sur la page
 	});
 });
 
-function loadJSON(file, callback) {   
-
-   	 var xobj = new XMLHttpRequest();
-   	 xobj.overrideMimeType("application/json");
-    	 xobj.open('GET', file, true);
-    	 xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            callback(xobj.responseText);
-          }
-   	 };
-    	xobj.send(null); 
-}
- 
 
 var map;
-function initMap(ville, obj) {
-       alert(obj[ville].lon);
-       initMap(obj[ville].lat,obj[ville].lon);
+function initMap(obj) {
+       initMap(obj.lat, obj.lon);
 }
 
 function initMap(lat,lng) {
